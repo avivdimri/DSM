3664ef2c06fa41bcab45358b9f73d875abd25b91
sayash <sayash@salesforce.com>
2022-04-27 18:12:53 +0300

    fix

--- my-app/src/main.js
+++ my-app/src/main.js
@@ -23,14 +23,3 @@ new Vue({
 }).$mount('#app')
 
 Vue.use(BootstrapVue);
-
-
-// import Vue from 'vue'
-// import App from './App.vue'
-// import router from './router'
-
-// Vue.config.productionTip = false
-// new Vue({
-//   router,
-//   render: h => h(App)
-// }).$mount('#app')
